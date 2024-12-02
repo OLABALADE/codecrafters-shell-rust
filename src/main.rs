a802025fe73b423ab99075fd8d35652f523563cb
Olabalade Emmanuel <olabaladeemmanuel@gmail.com>
2024-12-02 11:54:16 +0100

    exit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
 
 fn main() {
     // Uncomment this block to pass the first stage
@@ -12,8 +13,13 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        if input.len() != 0 {
-            println!("{}: command not found", input.trim());
+        let args: Vec<&str> = input.trim().split(" ").collect();
+        match args[0] {
+            "exit" => {
+                let code: i32 = args[1].parse().expect("Invalid code");
+                process::exit(code)
+            }
+            _ => println!("{}: command not found", input.trim()),
         }
     }
 }
