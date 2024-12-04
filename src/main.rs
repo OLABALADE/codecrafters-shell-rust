daecf4b6f4a0fb99b91786954648ebcb938b0088
Olabalade Emmanuel <olabaladeemmanuel@gmail.com>
2024-12-04 19:07:01 +0100

    pwd

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io::{self, Write};
 use std::{env, fs, process};
 
 fn main() {
-    let builtins = ["echo", "exit", "type"];
+    let builtins = ["echo", "exit", "type", "pwd"];
     let path = env::var("PATH").unwrap();
     let splits: Vec<&str> = path.as_str().split(":").collect();
 
@@ -22,35 +22,47 @@ fn main() {
 
             ["echo", ..] => println!("{}", args[1..].join(" ")),
 
-            ["type", cmd] => {
-                if builtins.contains(&cmd) {
-                    println!("{} is a shell builtin", cmd);
-                } else {
-                    if let Some(path) = splits
-                        .iter()
-                        .find(|path| fs::metadata(format!("{}/{}", path, cmd)).is_ok())
-                    {
-                        println!("{cmd} is {path}/{cmd}")
-                    } else {
-                        println!("{cmd}: not found")
-                    }
-                }
-            }
-            _ => {
-                if let Some(_) = splits
-                    .iter()
-                    .find(|path| fs::metadata(format!("{}/{}", path, args[0])).is_ok())
-                {
-                    let output = process::Command::new(args[0])
-                        .args(&args[1..])
-                        .output()
-                        .expect("Failed to process command");
+            ["type", cmd] => ty(&builtins, &splits, cmd),
 
-                    println!("{}", String::from_utf8_lossy(&output.stdout).trim())
-                } else {
-                    println!("{}: command not found", input.trim());
-                }
-            }
+            ["pwd", ..] => pwd(),
+
+            _ => check_path(&splits, &args),
         }
     }
+}
+
+fn ty(builtins: &[&str], splits: &Vec<&str>, cmd: &str) {
+    if builtins.contains(&cmd) {
+        println!("{} is a shell builtin", cmd);
+    } else {
+        if let Some(path) = splits
+            .iter()
+            .find(|path| fs::metadata(format!("{}/{}", path, cmd)).is_ok())
+        {
+            println!("{cmd} is {path}/{cmd}")
+        } else {
+            println!("{cmd}: not found")
+        }
+    }
+}
+
+fn check_path(splits: &Vec<&str>, args: &Vec<&str>) {
+    if let Some(_) = splits
+        .iter()
+        .find(|path| fs::metadata(format!("{}/{}", path, args[0])).is_ok())
+    {
+        let output = process::Command::new(args[0])
+            .args(&args[1..])
+            .output()
+            .expect("Failed to process command");
+
+        println!("{}", String::from_utf8_lossy(&output.stdout).trim())
+    } else {
+        println!("{}: command not found", args[0]);
+    }
+}
+
+fn pwd() {
+    let path = env::current_dir().unwrap();
+    println!("{}", path.display())
 }
